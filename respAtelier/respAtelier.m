a67fbe1694aec49084ddd5cac0e50fecbeb7ea32
Mehdi Kitane <mehdikitane@gmail.com>
2014-09-29 11:16:00 +0200

    Algo respAtelier

---
+++ respAtelier/respAtelier.m
@@ -0,0 +1,13 @@
+function [x] = respAtelier( A,B )
+%UNTITLED3 Summary of this function goes here
+%   Detailed explanation goes here
+
+%équation
+%f(a,b,c,d,e,f) = a+b+c+d+e+f (maximiser)
+
+f = [-1; -1; -1; -1; -1; -1];
+lb = [0;0;0;0;0;0];
+x = linprog(f, A, B,[],[],lb,[]); 
+
+end
+

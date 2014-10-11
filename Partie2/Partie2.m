68e9adf3856f78a809933cd925bda9e1139ca969
Mehdi Kitane <mehdikitane@gmail.com>
2014-10-11 20:26:08 +0200

    Correction Matrice

--- Partie2/Partie2.m
+++ Partie2/Partie2.m
@@ -1,12 +1,12 @@
 function [] = Partie2()
 
-F_compta = [-5.67; -12.38; -12.27; -1.03; -31.65; -27.55];
+F_compta = [-5.67; -11.88; -12.27; -1.03; -31.65; -27.55];
 F_respAtelier = [-1; -1; -1; -1; -1; -1];
 F_respStock = [-5; -5; -6; -10; -5; -4];
 F_respCom=[-1;-1;-1;1;1;1];
 F_respPers=[-13;-1;-11;-7;-20;-50];
 
-sol_compta = [0;23.51;0;0;242.5;87.96];
+sol_compta = [0;20.41;0;0;242.5;94.18];
 sol_respAtelier = [0;56.73;38.69;0;184.46;98.92];
 sol_respStock = [38.3473;25.4708;0.0000;0.0000;108.8663;130.3556];
 sol_respCom = [142.12;0;44.42;0;104.81;81.73];
@@ -17,7 +17,7 @@ Ft = [F_compta,F_respAtelier,F_respStock,F_respCom,F_respPers];
 Solt = [sol_compta,sol_respAtelier,sol_respStock,sol_respCom,sol_respPers];
 
 %Point de Mire : 
-PM = [10389; 378.8; 1385; 0; 2887.9]; 
+PM = [10512; 378.8; 1385; 0; 2887.9]; 
 Gain = -transpose(Solt)*Ft
 
 %Matrice de Satisfaction
@@ -34,7 +34,7 @@ A=[
     7 1 2 15 7 12;
     8 1 11 0 10 25;
     2 10 5 4 13 7;
-    5 0 0 7 10 27;
+    5 0 0 7 10 25;
     5 3 5 8 0 7;
     5 5 3 12 8 0;
     -1 -1 -1 -1 -1 -1; %Resp Atelier
@@ -57,7 +57,7 @@ sol_comptable = comptable(A,B);
 %Augmenter Personnel 2887.9
 
 %Point de Mire : 
-PM = [10389; 378.8; 1385; 0; 2887.9]; 
+PM = [10512; 378.8; 1385; 0; 2887.9]; 
 pointActuel = [-F_compta' * sol_comptable; -F_respAtelier' * sol_comptable; -F_respStock' * sol_comptable; -F_respCom' * sol_comptable; -F_respPers' * sol_comptable]
 
 
